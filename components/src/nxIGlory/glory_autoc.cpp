eda182abfc751468b42b60efe04637161ed62f23
shwneo@gmail.com <shwneo@gmail.com>
2014-04-17 00:11:46 +0800

    Doing some Python 'fake execution' stuff Add some comments

--- components/src/nxIGlory/glory_autoc.cpp
+++ components/src/nxIGlory/glory_autoc.cpp
@@ -3,6 +3,7 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/regex.hpp>
 #include <set>
+#include <string.h>
 #include "Scintilla.h"
 #include "SciLexer.h"
 #include "glory_autoc.h"
@@ -66,19 +67,49 @@ int show_modules_and_packages(string & word_list) {
 		string mod = * set_itor;
 		word_list += " ";
 		word_list += mod;
-		cout<<"[Module] "<<mod<<endl;
+		//cout<<"[Module] "<<mod<<endl;
 		++set_itor;
 	}
 }
 
+/* The heat of the `fake execution` mechanism */
+/* We will not execut every statment of the Python source */
+/* Sence there would be one or more `while 1` loop(s) in the source */
+/* That could cause the execut thread going to a infinite loop*/
+
+/* Sure to be excuted, report errors*/
+static regex reg_import("\\s*import\\s+");
+static regex reg_from_import("\\s*from\\s+");
+/* The assignment case is a little complicated: */
+/* If the r-value object (or some part of it) is an executable function, then don't execute this evaluate and
+   assign the l-value as an `None` object;
+   If the r-value object is an executable class object constructor, then don't execute this evaluate and 
+   log the l-value with the type name of the class object, the future `.` calling to the l-value would be
+   assign to the class object .
+   If the r-value object is NOT an executable object, then execut the evaluate happly, the virtual machine
+   would taking care of the syntax errors */
+static regex reg_assignment("\\s*(\\w+)\\s*=\\s*(\\.+)");
+static regex reg_def_function("\\s*def\\s+");
+static regex reg_def_classes("\\s*class\\s+");
+/* Must NOT been excuted! Look after the syntax errors ourself */
+static regex reg_while_loop("\\s*while\\s+");
+static regex reg_for_loop("\\s*for\\s+");
+/* Rest lines of the file */
+/* Setp 1: Try to use `hasattr('__call__')` to find out wether the object could be excuted*/
+/* Setp 2: Check the return value of `hasattr('__call__')`. If true, ignore it; 
+           If false or the calling been fault, execut the statment happly.
+           The virtual machine would taking care of the syntax errors */
+
 bool need_execute(string & line_statement) {
+	cout<<"[python] Seeing what to be done with:\n>>>"<<line_statement<<"\n"<<endl;
+
 	return false;
 }
 
 bool AutoCompleteManagerPython::ShowAutoComplete(char ch) {
 	char linebuf[512];
 	int current; 
-	int pos, line_num, edit_pos;
+	int pos, line_num, edit_pos, line_size;
 	int startword;
 	/* get neareast word */
 	if ( parent ) {
@@ -122,7 +153,18 @@ bool AutoCompleteManagerPython::ShowAutoComplete(char ch) {
 			edit_pos = parent->ngCommand(parent->ngInstance, SCI_GETCURRENTPOS, 0, 0);
 			line_num = parent->ngCommand(parent->ngInstance, SCI_LINEFROMPOSITION, edit_pos, 0);
 			cout<<"[python] ... new line : "<<line_num<<endl;
-			if ( need_execute(line_statement) ) {
+			line_size = parent->ngCommand(parent->ngInstance, SCI_GETLINE, line_num - 1, 0);
+			if ( line_size >= 512 ) {
+				cout<<"[python] long line not be processed"<<endl;
+				/* TODO: Re-alloc buffer for containing the whole line */
+				break;
+			}
+			//memset(linebuf, 0, 512);
+			line_size = parent->ngCommand(parent->ngInstance, SCI_GETLINE, line_num - 1, (int)linebuf);
+			cout<<"[python] get line size = "<<line_size<<endl;
+			linebuf[line_size] = '\0';
+			//cout<<"[python] processing line statement `"<<linebuf<<"`"<<endl;
+			if ( need_execute(string(linebuf)) ) {
 
 			}
 		break;
